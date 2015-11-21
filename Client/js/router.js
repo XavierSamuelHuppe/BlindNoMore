f6fdef46a552eebeac7749800ce805e6a210513f
carki <bofoc52@gmail.com>
2015-11-21 10:12:34 -0500

    just cleaning some things

---
+++ Client/js/router.js
@@ -0,0 +1,42 @@
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'views/home/HomeView'
+], function($, _, Backbone, HomeView) {
+
+    var AppRouter = Backbone.Router.extend({
+        routes: {
+            // Default
+            '*actions': 'defaultAction'
+        }
+    });
+
+    var initialize = function(){
+
+        Backbone.View.prototype.destroyView = function() {
+            this.undelegateEvents();
+            this.$el.empty();
+            delete this;
+        };
+
+        var viewCleanup = function (lastView) {
+            if(lastView) {
+                lastView.destroyView();
+            }
+        };
+
+        var app_router = new AppRouter;
+
+        app_router.on('route:defaultAction', function (actions) {
+            var homeView = new HomeView();
+            homeView.render();
+        });
+
+
+        Backbone.history.start();
+    };
+    return {
+        initialize: initialize
+    };
+});
