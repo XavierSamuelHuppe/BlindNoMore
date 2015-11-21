5ee372d0dd7342c6c1a7cc8fda08a8769c936084
XavierSamuelHuppe <xhuppe@gmail.com>
2015-11-21 08:54:57 -0500

    ajout du package.json

--- Serveur/app.js
+++ Serveur/app.js
@@ -1,12 +1,27 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
+app.use( bodyParser.json());
 
 app.get('/', function (req, res) {
 	res.send('Hello World!');
 });
+var emails = [];
+app.post('/login', function(req, res) {
+    var newEmail = req.body.email;
+    emails.push({email:newEmail});
+    console.log("added new email: "+ newEmail);
+});
+
+
+
+
+
+
 
 var server = app.listen(8080, function () {
 	var host = "127.0.0.1";
 	var port = server.address().port;
-	console.log("sup");
+	console.log("server started");
 });
